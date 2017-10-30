bcbd003f63e08cb1e973bec8b8e08f46e95bd589
WillForan <willforan@gmail.com>
2017-10-30 14:23:27 -0400

    init

---
+++ LNCDcal.py
@@ -0,0 +1,162 @@
+#from apiclient.discovery import build
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+import httplib2
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+
+import datetime,pytz
+import re
+import ConfigParser
+#"""
+# timezone/DST correction:
+#"""
+#def getTimeFromUTC() ):
+# #get the difference between localtime and now. round to half hours 
+# #N.B. bad b/c daylight saving!
+# secdiff=(datetime.datetime.utcnow() - datetime.datetime.now()).total_seconds()
+# hourdiff=round(secdiff/60,2)
+# return( datetime.timedelta(seconds=hourdiff*60) )
+#DELTAFROMUTC = getTimeFromUTC()
+ 
+ 
+def to_utc(dt,tzstr="America/New_York"):
+  tz=pytz.timezone(tzstr)
+  return(dt - tz.utcoffset(dt))
+  # later when printing, will get the same time as we put in
+  #utc=pytz.timezone('UTC')
+  #return( tz.localize(dt).astimezone( utc ) )
+
+#def to_tz(dt,tzstr="America/New_York"):
+#  tz=pytz.timezone(tzstr)
+#  utc=pytz.timezone('UTC')
+#  return( utc.localize(dt).astimezone( tz ) )
+
+
+def get_service(api_name, api_version, scope, key_file_location,
+                service_account_email):
+
+  credentials = ServiceAccountCredentials.from_p12_keyfile(
+    service_account_email, key_file_location, scopes=scope)
+
+  http = credentials.authorize(httplib2.Http())
+
+  # Build the service object.
+  service = build(api_name, api_version, http=http)
+
+  return service
+
+"""
+google time string to datetime
+-> google gives back time in localtime
+"""
+def g2time(dtstr):
+  return(datetime.datetime.strptime(dtstr[0:18],'%Y-%m-%dT%H:%M:%S'))
+"""
+get calendar info from google api returned dict
+split summary into expected parts: study age sex subj_initials ra score
+"""
+def calInfo(e):
+  d={
+   'start': e['start']['dateTime'],
+   'starttime': g2time(e['start']['dateTime']),
+   'dur_hr': (g2time(e['end']['dateTime']) - g2time(e['start']['dateTime'])).seconds/60/60,
+   'creator': e['creator'].get('displayName'),
+   'note': e.get('description'),
+   'calid': e.get('id'),
+   'summary': e.get('summary')
+  }
+
+  c=re.compile('(?P<study>[a-z/]+)[ -]*(?P<age>[0-9.]+) *yo(?P<sex>m|f) *\(?(?P<subjinit>[A-Z]{2,3})\)? *(?P<ra>[A-Z]{2,3})[ -]*(?P<score>[0-9.]+)',re.I)
+  m=re.search(c,e['summary'])
+  if m:
+    md=m.groupdict()
+    d={**d,**md}
+
+  return(d)
+
+
+def time2g(dt,tzstr="America/New_York"):
+ dtutc = to_utc(dt)
+ return( dtutc.isoformat() + 'Z' )
+
+def time2gdict(dt,tzstr="America/New_York"): return({'dateTime': time2g(dt), 'timeZone': tzstr})
+
+"""
+a class containing a connection to our google calendar 
+"""
+class LNCDcal():
+  # authetenticate
+  # ini: cal.ini
+  # [Calendar]
+  #  email = 'yyy@xxx.iam.gserviceaccount.com'
+  #  p12   = '/path/to/creds.p12'
+  #  calID = 'email@gmail.com'
+  #  tz    = 'America/New_York'
+  def __init__(self,ini):
+
+    # Define the auth scopes to request.
+    # -- read in from ini
+    config = ConfigParser.RawConfigParser()
+    config.read(ini)
+
+    service_account_email = config.get('Calendar','email')
+    key_file_location     = config.get('Calendar','p12')
+    self.calendarId       = config.get('Calendar','calID')
+    self.tzstr            = config.get('Calendar','tz')
+
+    scope = ['https://www.googleapis.com/auth/calendar']
+    self.cal = get_service('calendar', 'v3', scope, key_file_location,
+    service_account_email)
+    # might need to be updated after events are add
+    self.events = self.cal.events()
+
+  def find_in_range(self,dtmin,dtmax):
+    if( type(dtmin) == datetime.datetime ):
+       dtmin= time2g(dtmin)
+    if( type(dtmax) == datetime.datetime ):
+       dtmax= time2g(dtmax)
+
+    events = self.events.list(
+      calendarId=self.calendarId,
+      singleEvents=True,
+      timeMin=dtmin,
+      timeMax=dtmax).execute()
+    # use only events with datetime starts (remove full day events)
+    items = [ calInfo(i) for i in events['items'] if i['start'].get('dateTime')]
+    # check time
+    #dt.isoformat()[0:16] ==  items[0]['start']['dateTime'][0:16]
+    return(items)
+
+  def find(self,dt):
+    delta=10
+    dtmin=dt-datetime.timedelta(minutes=delta)
+    dtmax=dt+datetime.timedelta(minutes=delta)
+    items=self.find_in_range(dtmin,dtmax)
+    return(items)
+
+  def upcoming(self,daydelta=5):
+    dt=datetime.datetime.now()
+    dtmin=time2g(dt,self.tzstr)
+    dtmax=time2g(dt+datetime.timedelta(days=daydelta),self.tzstr)
+    items=self.find_in_range(dtmin,dtmax)
+    return(items)
+
+  def insert_event(self,startdt,dur_h,summary,desc):
+    endtime=startdt+datetime.timedelta(hours=dur_h)
+    event = {
+      'summary': summary,
+      'description': desc,
+      'start': time2gdict(startdt,self.tzstr),
+      'end': time2gdict(endtime,self.tzstr)
+    }
+    eventres = self.cal.events().insert(calendarId=self.calendarId,body=event).execute()
+    return(eventres)
+
+  def delete_event(self,eventId):
+    res = self.cal.events().delete(calendarId=self.calendarId,eventId=eventId).execute()
+    return(res)
+     
+  
---
+++ test_cal.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import pytest
+from gcal_serviceAccount import *
+import datetime
+
+def test_time2g_summer():
+  dt = datetime.datetime(2017,7,29,1,2,3)
+  gt = time2g(dt)
+  # utc is 4 hours later
+  assert(gt == "2017-07-29T05:02:03Z")
+
+def test_time2g_winter():
+  dt = datetime.datetime(2017,1,29,1,2,3)
+  gt = time2g(dt)
+  # utc is 5 hours later (dst)
+  assert(gt == "2017-01-29T06:02:03Z")
+
+def test_g2time():
+  gt="2017-01-29T10:05:04Z"
+  dt=g2time(gt)
+  # no seconds in google time
+  assert(dt == datetime.datetime(2017,1,29,10,5))
+
+def test_time2gdict():
+  pass
+
+class TestCal():
+  def setup(self):
+     self.cal = LNCDcal('cal.ini')
+
+  #insert_event(self,startdt,dur_h,summary,desc):
+  def test_insert_remove(self):
+     dt = datetime.datetime.now()
+     dur_h=2
+     e = self.cal.insert_event(dt,dur_h,'TEST ONLY','longer desc')
+     assert( e != None)
+     assert( e['id'] != None)
+     assert( e['summary'] == 'TEST ONLY')
+     assert( e['description'] == 'longer desc')
+
+     # google doesn't use seconds 
+     dtg=dt.replace(second=0,microsecond=0)
+     estarttime=g2time(e['start']['dateTime']).replace(second=0)
+     eendtime=g2time(e['end']['dateTime']).replace(second=0)
+     dtge = dt.replace(microsecond=0,second=0) + datetime.timedelta(hours=dur_h)
+
+     assert( dtg == estarttime )
+     assert( dtge == eendtime )
+
+
+     delres = self.cal.delete_event(e['id'])
+     assert(delres == '') 
+ 
